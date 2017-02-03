d3fffdfb53344439ef4f9aa9d58205a2740b60bb
balazs4 <balazs4web@gmail.com>
2017-02-03 16:42:23 +0100

    First plugins...

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const {resolve} = require('path');
 
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: resolve(__dirname, 'src', 'index.js'),
+    entry: {
+        app: resolve(__dirname, 'src', 'index.js')
+    },
     output: {
         path: resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    }
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'latest']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: mod => mod.context && mod.context.indexOf('node_modules') !== -1
+        }),
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            filename: 'index.html',
+            title: require('./package.json').name
+        }),
+    ]
 }
