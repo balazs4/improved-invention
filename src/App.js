ef27e1fbc15784a9ed1efdd15a713dc84f3e63db
balazs4 <balazs4web@gmail.com>
2017-02-05 21:52:05 +0100

    Prod/Dev webpack.config

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World!!!</h1>
-                <h3>Logo is sloooooow!</h3>
+                <h1>Hello World!!! YAY</h1>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 const {resolve} = require('path');
 
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const FrienlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
-
-module.exports = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        resolve(__dirname, 'src', 'index.js')
-    ],
+const app = {
+    entry: [resolve(__dirname, 'src', 'index.js')],
     output: {
-        path: resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        path: resolve(__dirname, 'dist')
     },
     module: {
         rules: [
@@ -33,10 +29,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
-        new FrienlyErrorsWebpackPlugin(),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
             minChunks: mod => mod.context && mod.context.indexOf('node_modules') !== -1
@@ -46,13 +39,69 @@ module.exports = {
             template: resolve(__dirname, 'src', 'index.html'),
             title: require('./package.json').name,
         }),
-        new FaviconsWebpackPlugin(resolve(__dirname, 'src', 'logo.png'))
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        quiet: true,
-        hot: true,
-        port: 3000,
-        contentBase: resolve(__dirname, 'dist')
+    ]
+}
+
+const mode = {
+    development: {
+        entry: [
+            'react-hot-loader/patch',
+            'webpack-dev-server/client?http://localhost:3000',
+            'webpack/hot/only-dev-server'
+        ],
+        output: {
+            filename: '[name].js'
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin(),
+            new FriendlyErrorsWebpackPlugin(),
+        ],
+        devtool: 'inline-source-map',
+        devServer: {
+            quiet: true,
+            hot: true,
+            port: 3000,
+            contentBase: resolve(__dirname, 'dist')
+        }
+    },
+    production: {
+        output: {
+            filename: '[name].[hash].js'
+        },
+        plugins: [
+            new FaviconsWebpackPlugin(resolve(__dirname, 'src', 'logo.png')),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }),
+            new webpack.optimize.AggressiveMergingPlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                mangle: true,
+                compress: {
+                    warnings: false,
+                    pure_getters: true,
+                    unsafe: true,
+                    unsafe_comps: true,
+                    screw_ie8: true
+                },
+                output: {
+                    comments: false,
+                },
+                exclude: [/\.min\.js$/gi] // skip pre-minified libs
+            }),
+            new CompressionWebpackPlugin({
+                asset: "[path].gz[query]",
+                algorithm: "gzip",
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0
+            })
+        ],
+        devtool: 'source-map',
     }
 }
+
+module.exports = merge.smart(app, mode[process.env.NODE_ENV || 'development']);
