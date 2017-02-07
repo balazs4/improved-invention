7c2ebdc45c3aad058b8c147a977dc6f379c4db7f
balazs4 <balazs4web@gmail.com>
2017-02-07 17:04:28 +0100

    change text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World!!! native react</h1>
+                <h1>Hello World!!! preact</h1>
             </div>
         );
     }
